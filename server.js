7a44a8a53795930946699df16ae1410c833f8678
Mushabrahman <83585774+Mushabrahman@users.noreply.github.com>
2023-03-29 23:29:55 +0530

    small changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")))
+// app.use(express.static(path.join(__dirname, "./client/build")))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -32,12 +32,12 @@ app.use("/api/v1/product", productRoutes);
 
 
 //rest api
-app.use("*", function(req,res){
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-})
-// app.get("/",(req,res)=>{
-//   res.send("<h1>Welcome</h1>");
+// app.use("*", function(req,res){
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // })
+app.get("/",(req,res)=>{
+  res.send("<h1>Welcome</h1>");
+})
 //PORT
 const PORT = process.env.PORT || 8080;
 
